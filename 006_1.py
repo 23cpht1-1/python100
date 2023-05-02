2ab1f9230be014c39c284e4e87d0a2c61cb143db
CPHT23-1 <128008396+poly2305341001@users.noreply.github.com>
2023-05-02 21:00:58 +0900

    Rename 006_1.py to D006_1.py

--- 006_1.py
+++
@@ -1,8 +0,0 @@
-age = int(input("당신의 나이는?"))
-
-left_years = 90 - age
-left_months = left_years*12
-left_weeks = left_years*52
-left_days = left_weeks*365
-
-print(f"90살까지 당신의 남은 날은 {left_days}일, 남은 주는 {left_weeks}주, 남은 달은 {left_months}, 남은 해는 {left_years}해입니다. 준비하쎄욧!")
